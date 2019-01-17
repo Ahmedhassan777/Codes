64a4592cbf0c97f3bdadde333c2318114da01026
Ahmedhassan777 <37689522+Ahmedhassan777@users.noreply.github.com>
2019-01-17 10:57:44 +0100

    Create code.ino
    check the folder creation

---
+++ first example/code.ino
@@ -0,0 +1,7 @@
+#define PROJECT_NAME "BuzzerMega"
+#define VERSION "v0.2.0"
+
+#include "includes.h"
+#include "song.h"
+////////CONSTANTS////////
+//In this section, all constants resulting from the prototype board design are set
